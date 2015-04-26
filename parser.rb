f5bc7a93ad7cd78237e4a8cfaee48798805a4966
Nicolo Benigni <fvbenigni@gmail.com>
2015-04-26 16:08:15 +0200

    add: OPS parser home/away

--- parser.rb
+++ parser.rb
@@ -18,7 +18,7 @@ end
 
 today_matches = Hash.new 
 
-# collect data for each match
+# collect data for each match and return an hash of them (today_matches)
 urls.each do |url|
 
     preview = Nokogiri::HTML(open(url))
@@ -49,12 +49,45 @@ urls.each do |url|
         match_data["record_pitcher_home"] = table_home.css(".blank_table")[1].css("+ tr td")[0].next_element.next_element.content
         match_data["era_pitcher_home"] = table_home.css(".blank_table")[1].css("+ tr td")[10].content
     end 
-
+    
     
 
+    def extract_OPS_data(table)
+        rows = table.css("tr")
+        rows.delete rows.first
+        counter = 0
+
+        values = Array.new
+
+        (0..12).each do |index|
+            value =  rows[index].css("td")[1].content
+            
+            if value =~ Regexp.new('.*/')
+                
+                val = value.sub(Regexp.new('.*/'), '').to_f
+                if val != 0
+                    values << val
+                    counter = counter + 1
+                end
+            else
+                # do nothing
+            end
+        end
+        average = values.inject{ |sum, el| sum + el.to_f }.to_f / values.size
+        return average, counter
+    end
+
+    tables = preview.css(".x_small_text.border")
+
+    table_away = tables.pop
+    table_home = tables.pop
+    match_data["OPS_away"], match_data["counter_OPS_away"] = extract_OPS_data table_away
+    match_data["OPS_home"], match_data["counter_OPS_home"] = extract_OPS_data table_home
+    ap today_matches
+
+    exit # evita di farlo per ogni match, da togliere dopo debug
 
 
-    ap match_data
-    ap "-----"
 end
 
+# ap today_matches
