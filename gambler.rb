142f8a3acf5c88de1f4b2844fff0fd4595e89ad3
Nicolo Benigni <fvbenigni@gmail.com>
2015-04-27 19:12:57 +0200

    add: logger of matches

--- gambler.rb
+++ gambler.rb
@@ -1,19 +1,20 @@
 class Gambler
     attr_accessor :matches
 
-    def initialize today_matches
+    def initialize today_matches, logger
         @matches = today_matches
+        @logger = logger
     end
 
     def predict_a_class
-        
+        count = 0
         self.matches.each do |match|
             name = match[0]
             match = match[1]
             if ((match["era_pitcher_home"]) && (match["era_pitcher_away"]) && (match["era_pitcher_home"] < 3) && (match["era_pitcher_away"] > 3 ))
                 if ((match["OPS_home"]) && (match["OPS_away"]) && (match["OPS_home"] > 0.800 && match["counter_OPS_home"] > 7) && (match["OPS_away"] < 0.750 && match["counter_OPS_away"] >= 7))
                     p name
-                    p "yes"
+                    count += 1
                 else
                     next
                 end
@@ -21,5 +22,7 @@ class Gambler
                 next
             end
         end
+
+        @logger.info "Nothing on #{DateTime.now.strftime("%F")}"if count == 0
     end
 end
--- main.rb
+++ main.rb
@@ -3,10 +3,13 @@
 require 'nokogiri'
 require 'open-uri'
 require 'awesome_print'
+require 'logger'
 require_relative 'parser'
 require_relative 'gambler'
 
-p = Parser.new 
-today_matches = p.parse
-g = Gambler.new today_matches
-g.predict_a_class
+logger = Logger.new 'matches.log', 'weekly'
+
+parser = Parser.new 
+today_matches = parser.parse
+gambler = Gambler.new today_matches, logger
+gambler.predict_a_class
