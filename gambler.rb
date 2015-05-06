4aaf0eb76fd47b61a97ed37f368bdbc05d7a08fe
Nicolo Benigni <fvbenigni@gmail.com>
2015-05-06 20:07:19 +0200

    add: script. start: plan_B

--- gambler.rb
+++ gambler.rb
@@ -13,7 +13,7 @@ class Gambler
             match = match[1]
             if ((match["era_pitcher_home"]) && (match["era_pitcher_away"]) && (match["era_pitcher_home"] < 3) && (match["era_pitcher_away"] > 3 ))
                 if ((match["OPS_home"]) && (match["OPS_away"]) && (match["OPS_home"] > 0.800 && match["counter_OPS_home"] > 7) && (match["OPS_away"] < 0.750 && match["counter_OPS_away"] >= 7))
-                    p name
+                    @logger.info "A class match detected on #{DateTime.now.strftime("%F")} --> #{name}"
                     count += 1
                 else
                     next
